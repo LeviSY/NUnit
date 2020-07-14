d719b48a03584ab0e1840a1b7c6bf3729eaa143c
LeviSY <49865069+LeviSY@users.noreply.github.com>
2020-07-14 20:59:10 +0300

    Add files via upload

---
+++ NUnit/NUnit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nunit
+{
+    public static class NUnitTest
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static double sum(double a, double b)
+        {
+            return a + b;
+        }
+        public static double sub(double a, double b)
+        {
+            return a - b;
+        }
+        public static double mul(double a, double b)
+        {
+            return a * b;
+        }
+        public static double div(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ NUnit/NUnitTests/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace NUnitTests
+{
+    public class Class1
+    {
+        double x = 6;
+        double y = 4;
+        [Test]
+        public void Test1()
+        {
+            double xy = x + y;
+            Assert.AreEqual(nunit.NUnitTest.sum(x, y), xy);
+        }
+        [Test]
+        public void Test2()
+        {
+            double xy = x - y;
+            Assert.AreEqual(nunit.NUnitTest.sub(x, y), xy);
+        }
+        [Test]
+        public void Test3()
+        {
+            double xy = x * y;
+            Assert.AreEqual(nunit.NUnitTest.mul(x, y), xy);
+        }
+        [Test]
+        public void Test4()
+        {
+            double xy = x / y;
+            Assert.AreEqual(nunit.NUnitTest.div(x, y), xy);
+        }
+
+    }
+}
